de6051d2db671f4fc371906a93701608cd30f619
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-11-09 20:16:30 +0530

    day-1

--- mailhubb/src/Pages/Signup.jsx
+++ mailhubb/src/Pages/Signup.jsx
@@ -9,13 +9,13 @@ function Signup() {
     const [show, setShow] = React.useState(false)
     const handleClick = () => setShow(!show)
     return <>
-        <Box width='100%' h='100vh' display='flex' borderRight='30rem solid #ffe200'>
+        <Box width='100%' h='full' display='flex' borderRight='30rem solid #ffe200'>
 
             <Link className='Link' href='/'>
                 <Image w='105px' h='105px' src='https://lh3.googleusercontent.com/ep0W-AqQRFlDsvJppYegbZb09v7qmuXPSJRnokB6QZDM9K1hW6wxhYuHbFd5_ffDX3301tHC_y5pJVFlNK5U0APG5qFIG3hdFOTTOYlmSUkDPrE1wI24780s8jPA-UV9NbRmaxwmOhyiBF4byU_qg6SxJ-f4UrY0sHbmUTJILW8Wj9wr94CJzgxWZz-TqYb9JMdgmOS7mWuUXnhaOp6ZK4KjYrtN-3hGLHeWvRLQo598ofKzUaRj-YA7z0sZP_lh9VGHr9yW1PQ530TlW2NXV2d-UrXQCEkHm0m8PfSTKf0uJcM_xuoSxGHZN1sAZZ4zALSX_4InItRVLyyaw01Wg2iSuyZJbwhylxqGKFBj5jHK2tgNVlN-KFEPEX-rciN9MLzww7fzDHka7SVlQhKuLI29koUsXA12Bbu4d9OD3R3YskZTojLwDURY1Mbw3eBU2A8U9jwAzGw64mYP58BorjkkTlMNqE8YyfL3gTAk4lUTAbED8gs-jcInuQpAKB9HgB2ZjeSN1cGI5UCiw348LpmeSUesq7VuLjR5hrW3TrgqPeSNVh_fWUFM7eGVKS1z4d8o-RxznAtFe6fJuD3LBEfdvWVy1KQXCE_dU38R7aF9blPFuTiU5FstbqffP16-sjab094R88QKuWkXLZV3hSua9ZihQm73xC0zPEus3Hi3Cej2SU71j5OqL8uVJzKLaLoEg2EYBaJVZMxnksxdUUP2sugDkf_2K6A7TTAV8BipUttorpEMHKf0DakEPOifYiTuTQAYc1nZQZLc3cTxXViSvypqnTFtEmqUHMtwapCDq2bq3jUAM-VBc7g4965fQG1FZ-zIbCuKlHod_EK2nfeyJ-5pUvLk5_G7iYiIOUiuBI2E62xJtxyPdxIjaYPUWhgOMwnGQshYw4y6qc3rKLZvqTlfT3AGSHOjyIH3_GeM_bYV8TpBabBQ1JyA5VGLYc3UgdgaMytd-B-YxtmFScfnb6ry9GR0TvrWZFEiFhS1cDRjvNvr7g=s500-no?authuser=0'></Image>
             </Link>
-            <Box display='flex' w='100%' flexDirection='row'  >
-                <Box w='50%' pr='5rem'>
+            <Box display='flex' w='100%' h='full'   flexDirection='row'  >
+                <Box w='50%' h='full' m='10rem auto 0'>
                     <Heading textAlign='start' fontFamily='mono' fontSize='32px'>Sign up for Mailchimp</Heading>
                     <Text textAlign='start' mb={5}>Create a free account or <Link color='blue' className='Link' href="/login" sx={{
                         '.Link:hover &': {
@@ -46,7 +46,7 @@ function Signup() {
                         </Stack>
                         <Text textAlign='start' mt='1rem'>Invisible reCAPTCHA by Google <Link color='blue' textDecoration='underline' href="#">Privacy Policy</Link> and <Link color='blue' textDecoration='underline' href="#">Terms of Use.</Link></Text>
                     </FormControl>
-                    <Box mt="30%">
+                    <Box mt="18%" ml='-53%'>
                         <Text fontSize={13} textAlign="start" color="gray.500">©2001–2022 All Rights Reserved. Mailchimp® is a registered trademark of The Rocket Science Group, LLC.</Text>
                         <Text fontSize={13} textAlign="start" color="gray.500"><Link color='blue' textDecoration='underline' href="#">Cookie Preferences</Link> , <Link color='blue' textDecoration='underline' href="#">Privacy</Link> and <Link color='blue' textDecoration='underline' href="#">Terms</Link></Text>
                     </Box>
